90b8f9d28f26e92b2006eb082509ef920f4e3fb7
Nigel <nigelawj@gmail.com>
2020-04-14 14:42:43 +0800

    initalised middlewares, refactored routes into controllers

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const express = require('express');
+const app = express();
+
 const dotenv = require('dotenv');
+// const logger = require('./middleware/logger'); // Simple middleware for testing purposes
+const morgan = require('morgan');
 
 // Load config
 dotenv.config({ path: './config/config.env' });
-
-const app = express();
 const PORT = process.env.PORT || 5000;
+
+// Dev Logging Middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// Routes
+app.use('/api/v1/bootcamps', require('./routes/bootcamps'));
 
 app.listen(
   PORT,
